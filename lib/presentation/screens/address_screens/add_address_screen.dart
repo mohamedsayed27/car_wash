c1634a5ba483425251acfdd35f240802653faf70
mohamed sayed <mosayed.ezt@gmail.com>
2024-04-03 15:43:38 +0200

    changes

--- lib/presentation/screens/address_screens/add_address_screen.dart
+++ lib/presentation/screens/address_screens/add_address_screen.dart
@@ -82,6 +82,7 @@ class AddAddressScreen extends StatelessWidget {
                         hintText: "الدور",
                       ),
                     ),
+
                     CustomSizedBox(
                       width: 8,
                     ),
